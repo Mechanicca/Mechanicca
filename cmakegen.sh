0e504c382be781a7421972efd159a40523f5f481
Martin Kopecký <martin.monster696@gmail.com>
2017-07-13 12:26:56 +0200

    Mechanicca root project structure setup

---
+++ cmakegen.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# Purpose: Prepare CMake build for current project
+# Author: Martin Kopecky <martin.monster696@gmail.com> 
+
+DIRECTORY=Build
+
+if [ ! -d "$DIRECTORY" ]; then
+	echo "Making build directory..."
+	mkdir ./$DIRECTORY
+	echo "done"
+fi
+
+echo "Configuring CMake build"
+cd ./$DIRECTORY && cmake ../
+
